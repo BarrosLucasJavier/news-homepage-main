b447c7e7764762d2ba3b12c51c998b126b8e43b5
Barros Lucas Javier <lucasbcco@gmail.com>
2024-05-30 19:36:04 -0300

    desktop version done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,11 @@ window.onload = () =>{
     const iconOpen = document.getElementById('iconOpen');
     const iconClose = document.getElementById('iconClose');
     const navbar = document.getElementById('navbar');
+    const webImg = document.getElementById('webImg');
     
+    if (window.innerWidth > 768) {
+        webImg.src="./assets/images/image-web-3-desktop.jpg"
+    }
     iconOpen.addEventListener('click', ()=>{
         navbar.classList.add('showNav');
     });
